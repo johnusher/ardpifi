c2359d470658b9e9d9145bdc6ba7cdf3dd9b0132
John Usher <yesjhon@gmail.com>
2021-01-03 20:32:33 +0100

    commit 3

--- jumain.go
+++ jumain.go
@@ -1,3 +1,5 @@
+// public
+
 package main
 
 import (
@@ -13,7 +15,8 @@ func main() {
 	defer logger.FinalizeLogger()
 	fmt.Println("started i2c'ing")
 
-	// Create new connection to I2C bus on 2 line with address 0x27
+	// Create new connection to Arduino:
+	// I2C bus on line 1 with address 0x08
 	i2c, err := i2c.NewI2C(0x08, 1)
 	if err != nil {
 		log.Fatal(err)
@@ -32,6 +35,8 @@ func main() {
 	// 	log.Fatal(err)
 	// }
 	//i2c.Write([]byte{0x1, 0xF3})
+
+	// write data to I2C
 	i2c.WriteBytes([]byte{0x1, 0xF3})
 
 	// Uncomment/comment next line to suppress/increase verbosity of output
