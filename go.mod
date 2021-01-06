403558f90679fb36b65af92e5ba56d2fc33aec9f
John Usher <yesjhon@gmail.com>
2021-01-06 12:00:32 +0100

    serial USB updates

--- go.mod
+++ go.mod
@@ -5,11 +5,13 @@ go 1.15
 require (
 	github.com/d2r2/go-i2c v0.0.0-20191123181816-73a8a799d6bc
 	github.com/d2r2/go-logger v0.0.0-20181221090742-9998a510495e
-	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/huin/goserial v0.0.0-20121012073615-7b90efdb22b1
 	github.com/kr/text v0.2.0 // indirect
 	github.com/nsf/termbox-go v0.0.0-20201124104050-ed494de23a00
 	github.com/sirupsen/logrus v1.7.0
 	github.com/stretchr/testify v1.6.1 // indirect
+	github.com/tarm/serial v0.0.0-20180830185346-98f6abe2eb07
+
 	golang.org/x/sys v0.0.0-20210105210732-16f7687f5001 // indirect
 	gopkg.in/check.v1 v1.0.0-20201130134442-10cb98267c6c // indirect
 	gopkg.in/yaml.v3 v3.0.0-20210105161348-2e78108cf5f8 // indirect
--- jumain.go
+++ jumain.go
@@ -6,18 +6,45 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
 	"os"
 	"os/signal"
+	"strings"
 	"syscall"
+	"time"
 
 	"github.com/d2r2/go-i2c"
 	"github.com/d2r2/go-logger"
+
 	log "github.com/sirupsen/logrus"
+	"github.com/tarm/serial"
 
 	"github.com/johnusher/ardpifi/pkg/keyboard"
 )
 
 func main() {
+
+	// Find the device that represents the arduino serial
+	// connection.
+
+	c := &serial.Config{Name: findArduino(), Baud: 9600, ReadTimeout: time.Second * 1}
+
+	s, err := serial.OpenPort(c)
+	if err != nil {
+		log.Errorf("failed to find Arduino: %s", err)
+		return
+	}
+
+	// When connecting to an older revision Arduino, you need to wait
+	// a little while it resets.
+	time.Sleep(1 * time.Second)
+
+	n, err := s.Write([]byte("0"))
+	if err != nil {
+		log.Errorf("failed to write to port: %s", err)
+		return
+	}
+
 	stop := make(chan os.Signal, 1)
 	signal.Notify(stop, os.Interrupt, syscall.SIGTERM)
 
@@ -45,7 +72,20 @@ func main() {
 				return
 			}
 			log.Infof("key pressed: %s / %d / 0x%X / 0%o", string(key), key, key, key)
-			// n, err := i2c.WriteBytes([]byte{0x1, 0xF3})
+
+			n, err = s.Write([]byte(string(key)))
+			if err != nil {
+				log.Errorf("2. failed to write to serial port: %s", err)
+				return
+			}
+
+			buf := make([]byte, 128)
+			n, _ = s.Read(buf)
+			if err != nil {
+				log.Errorf("serial port read error, %s", err)
+			}
+			log.Print("%q", buf[:n])
+
 		}
 	}
 
@@ -81,7 +121,7 @@ func main() {
 	// ....
 
 	// write data to I2C
-	n, err := i2c.WriteBytes([]byte{0x1, 0xF3})
+	n, err = i2c.WriteBytes([]byte{0x1, 0xF3})
 	if err != nil {
 		log.Errorf("failed to WriteBytes: %s", err)
 		return
@@ -98,3 +138,77 @@ func main() {
 
 	fmt.Println("ended")
 }
+
+// findArduino looks for the file that represents the Arduino
+// serial connection. Returns the fully qualified path to the
+// device if we are able to find a likely candidate for an
+// Arduino, otherwise an empty string if unable to find
+// something that 'looks' like an Arduino device.
+func findArduino() string {
+	contents, _ := ioutil.ReadDir("/dev")
+
+	// Look for what is mostly likely the Arduino device
+	// JU: on my RASPI it shows in ttyAMA0
+	for _, f := range contents {
+		if strings.Contains(f.Name(), "tty.usbserial") ||
+			strings.Contains(f.Name(), "ttyUSB") || strings.Contains(f.Name(), "ttyAMA0") {
+			fmt.Println("Duino found at /dev/", f.Name())
+			return "/dev/" + f.Name()
+		}
+	}
+
+	// Have not been able to find a USB device that 'looks'
+	// like an Arduino.
+	return ""
+}
+
+// sendArduinoCommand transmits a new command over the nominated serial
+// port to the arduino. Returns an error on failure. Each command is
+// identified by a single byte and may take one argument (a float).
+// func sendArduinoCommand(
+// 	command byte, serialPort io.ReadWriteCloser) error {
+// 	if serialPort == nil {
+// 		return nil
+// 	}
+
+// 	// // Package argument for transmission
+// 	// bufOut := new(bytes.Buffer)
+// 	// err := binary.Write(bufOut, binary.LittleEndian, argument)
+// 	// if err != nil {
+// 	// 	return err
+// 	// }
+
+// 	// Transmit command and argument down the pipe.
+// 	for _, v := range []byte{command} {
+// 		_, err = serialPort.Write(v)
+// 		if err != nil {
+// 			return err
+// 		}
+// 	}
+
+// 	return nil
+// }
+
+// func sendArduinoCommand(
+// 	command byte, argument float32, serialPort io.ReadWriteCloser) error {
+// 	if serialPort == nil {
+// 		return nil
+// 	}
+
+// 	// Package argument for transmission
+// 	bufOut := new(bytes.Buffer)
+// 	err := binary.Write(bufOut, binary.LittleEndian, argument)
+// 	if err != nil {
+// 		return err
+// 	}
+
+// 	// Transmit command and argument down the pipe.
+// 	for _, v := range [][]byte{[]byte{command}, bufOut.Bytes()} {
+// 		_, err = serialPort.Write(v)
+// 		if err != nil {
+// 			return err
+// 		}
+// 	}
+
+// 	return nil
+// }
