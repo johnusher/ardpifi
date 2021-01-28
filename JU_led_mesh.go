38e5c3e0b9ac07a286dfe468ab2cfe9c445d1768
John Usher <yesjhon@gmail.com>
2021-01-28 10:11:33 +0100

    gps distance

--- JU_led_mesh.go
+++ JU_led_mesh.go
@@ -308,8 +308,11 @@ func messageLoop(messages <-chan []byte, duino port.Port, raspID string, lcd lcd
 				long2 := crwt.Longf
 
 				bearing, _ := calcGPSBearing(lat1, long1, lat2, long2)
+				disance := calcGPSdistance(lat1, long1, lat2, long2)
 
-				log.Infof("GPS bearing to ID %s is %f", crwt.ID, bearing)
+				log.Infof("GPS bearing to ID %s is %f\xB0", crwt.ID, bearing)
+
+				log.Infof("GPS distance to ID %s is %f metres", crwt.ID, disance)
 			}
 		}
 
@@ -338,7 +341,6 @@ func messageLoop(messages <-chan []byte, duino port.Port, raspID string, lcd lcd
 				// write to duino:
 				duino.Flush()
 				_, err := duino.Write([]byte(string(jsonMessage.Key)))
-				// message[4] gives me the letter "t", perhaps as message = {Latf:52.534587 Longf:13.347233 ID:raspi 1 Key:0}
 
 				if err != nil {
 					log.Errorf("3. failed to write to serial port: %s", err)
@@ -497,3 +499,37 @@ func calcGPSBearing(lat1 float64, long1 float64, lat2 float64, long2 float64) (f
 }
 
 // fmt.Sprintf("%.6f", decimal), nil
+
+// distance between two points.
+// from https://gist.github.com/cdipaolo/d3f8db3848278b49db68
+
+// haversin(θ) function
+func hsin(theta float64) float64 {
+	return math.Pow(math.Sin(theta/2), 2)
+}
+
+// Distance function returns the distance (in meters) between two points of
+//     a given longitude and latitude relatively accurately (using a spherical
+//     approximation of the Earth) through the Haversin Distance Formula for
+//     great arc distance on a sphere with accuracy for small distances
+//
+// point coordinates are supplied in degrees and converted into rad. in the func
+//
+// distance returned is METERS!!!!!!
+// http://en.wikipedia.org/wiki/Haversine_formula
+func calcGPSdistance(lat1, lon1, lat2, lon2 float64) float64 {
+	// convert to radians
+	// must cast radius as float to multiply later
+	var la1, lo1, la2, lo2, r float64
+	la1 = lat1 * math.Pi / 180
+	lo1 = lon1 * math.Pi / 180
+	la2 = lat2 * math.Pi / 180
+	lo2 = lon2 * math.Pi / 180
+
+	r = 6378100 // Earth radius in METERS
+
+	// calculate
+	h := hsin(la2-la1) + math.Cos(la1)*math.Cos(la2)*hsin(lo2-lo1)
+
+	return 2 * r * math.Asin(math.Sqrt(h))
+}
