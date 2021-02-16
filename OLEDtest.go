e765607c2137127cc852d524c1a7fe8c55020296
John Usher <yesjhon@gmail.com>
2021-02-16 17:31:48 +0100

    OLED testin

--- OLEDtest.go
+++ OLEDtest.go
@@ -2,34 +2,57 @@ package main
 
 import (
 	"image"
-	"os"
 
+	"golang.org/x/image/font"
+	"golang.org/x/image/font/basicfont"
+	"golang.org/x/image/math/fixed"
+
+	"image/color"
 	_ "image/png"
 
-	oled_i2c "github.com/johnusher/ardpifi/pkg/OLED"
+	"time"
 
+	"github.com/goiot/devices/monochromeoled"
 	"golang.org/x/exp/io/i2c"
 )
 
 func main() {
-	rc, err := os.Open("./golang.png")
-	if err != nil {
-		panic(err)
-	}
-	defer rc.Close()
 
-	m, _, err := image.Decode(rc)
-	if err != nil {
-		panic(err)
-	}
+	img := image.NewRGBA(image.Rect(0, 0, 128, 64))
+	addLabel(img, 20, 30, "Maxii!!")
 
-	// d, err := monochromeoled.Open(&i2c.Devfs{Dev: "/dev/i2c-1"})
-	d, err := oled_i2c.Open(&i2c.Devfs{Dev: "/dev/i2c-1"})
+	addLabel(img, 20, 60, "daddy!!")
 
+	// f, err := os.Create("out.png")
+	// if err != nil {
+	// 	panic(err)
+	// }
+	// defer f.Close()
+	// if err := png.Encode(f, img); err != nil {
+	// 	panic(err)
+	// }
+
+	// rc, err := os.Open("./maxi.png")
+
+	// if err != nil {
+	// 	panic(err)
+	// }
+	// defer rc.Close()
+
+	// m, _, err := image.Decode(rc)
+	// if err != nil {
+	// 	panic(err)
+	// }
+
+	m := img
+
+	d, err := monochromeoled.Open(&i2c.Devfs{Dev: "/dev/i2c-1"})
 	if err != nil {
 		panic(err)
 	}
 	defer d.Close()
+
+	time.Sleep(2 * time.Second)
 
 	// clear the display before putting on anything
 	if err := d.Clear(); err != nil {
@@ -41,4 +64,17 @@ func main() {
 	if err := d.Draw(); err != nil {
 		panic(err)
 	}
+}
+
+func addLabel(img *image.RGBA, x, y int, label string) {
+	col := color.RGBA{200, 100, 0, 255}
+	point := fixed.Point26_6{fixed.Int26_6(x * 64), fixed.Int26_6(y * 64)}
+
+	d := &font.Drawer{
+		Dst:  img,
+		Src:  image.NewUniform(col),
+		Face: basicfont.Face7x13,
+		Dot:  point,
+	}
+	d.DrawString(label)
 }
--- go.mod
+++ go.mod
@@ -6,9 +6,13 @@ require (
 	github.com/d2r2/go-hd44780 v0.0.0-20181002113701-74cc28c83a3e
 	github.com/d2r2/go-i2c v0.0.0-20191123181816-73a8a799d6bc
 	github.com/d2r2/go-logger v0.0.0-20181221090742-9998a510495e
+	github.com/fogleman/gg v1.3.0 // indirect
+	github.com/goiot/devices v0.0.0-20160708214026-09d1226fc8ea // indirect
+	github.com/golang/freetype v0.0.0-20170609003504-e2365dfdc4a0 // indirect
 	github.com/gorilla/websocket v1.4.2
 	github.com/huin/goserial v0.0.0-20121012073615-7b90efdb22b1
 	github.com/jacobsa/go-serial v0.0.0-20180131005756-15cf729a72d4
+	github.com/jdevelop/golang-rpi-extras v0.0.0-20181010003844-c75e8edb0d6f
 	github.com/kpeu3i/bno055 v1.0.3 // indirect
 	github.com/kr/text v0.2.0 // indirect
 	github.com/nsf/termbox-go v0.0.0-20201124104050-ed494de23a00
@@ -18,6 +22,7 @@ require (
 	github.com/sirupsen/logrus v1.7.0
 	github.com/stretchr/testify v1.6.1 // indirect
 	github.com/tarm/serial v0.0.0-20180830185346-98f6abe2eb07
+	golang.org/x/exp v0.0.0-20210212053707-62dc52270d37
 	golang.org/x/sys v0.0.0-20210105210732-16f7687f5001 // indirect
 	gopkg.in/check.v1 v1.0.0-20201130134442-10cb98267c6c // indirect
 	gopkg.in/yaml.v3 v3.0.0-20210105161348-2e78108cf5f8 // indirect
