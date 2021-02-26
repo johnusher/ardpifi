61fa21796d277b1b6cbd2e91da219ef2ffd1102f
John Usher <yesjhon@gmail.com>
2021-02-26 10:35:02 +0100

    process euler

---
+++ processEuler.m
@@ -0,0 +1,40 @@
+
+clear all
+ca
+
+cd('C:\Users\john\Dropbox\skitz')
+fn = 'eulerSwift_data.txt'
+X=importdata(fn,'%s');
+
+% X = cellfun(X,3);
+
+bsf = 0;    % bearingScaleFlag
+
+for i = 26:length(X)*0.9
+    b(i,:) = str2num(cell2mat(X(i)));    
+end
+x = b(:,1);
+y = b(:,2);
+z = b(:,3);
+
+x=unwrap(x,80);
+
+
+figure;plot(b)
+legend('x','y','z')
+
+
+%  dataout =  iomega(datain, dt, datain_type, dataout_type)
+
+% dataoutX =  iomega(x, 0.1, 3, 1);
+% dataoutY =  iomega(y, 0.1, 3, 1);
+% dataoutZ =  iomega(z, 0.1, 3, 1);
+
+% figure;plot(x);hold on;plot(dataoutX,'x'); title('bearin')
+% figure;plot(y);hold on;plot(dataoutY,'x'); title('roll')
+% figure;plot(z);hold on;plot(dataoutZ,'x'); title('tilt')
+
+
+figure;plot(x); title('bearin')
+figure;plot(y); title('roll')
+figure;plot(z); title('tilt')
