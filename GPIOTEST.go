a73af454b775fbffcb2453fe392a1062166f33e8
John Usher <yesjhon@gmail.com>
2021-05-03 00:08:32 +0200

    GPIO switch debouncing

--- GPIOTEST.go
+++ GPIOTEST.go
@@ -4,30 +4,60 @@ package main
 
 import (
 	"fmt"
-	"log"
+	// "log"
 	"os"
 	"os/exec"
 	"syscall"
 	"time"
 
+	log "github.com/sirupsen/logrus"
+
 	"github.com/warthog618/gpiod"
-	"github.com/warthog618/gpiod/device/rpi"
 )
 
-func eventHandler(evt gpiod.LineEvent) {
+const (
+	bounceTime = 150 * time.Millisecond
+)
+
+type buttonPress struct {
+	lastMessageTypeReceived string
+	lastButtonEventType     string
+	lastMessageReceived     time.Time
+}
+
+var buttonTimes buttonPress // have to make this a global!!
+
+func buttonEventHandler(evt gpiod.LineEvent) {
+	oldTime := buttonTimes.lastMessageReceived
 	t := time.Now()
+
+	timeDiff := t.Sub(oldTime)
+
+	if timeDiff < bounceTime {
+		// log.Info("TOO QQUICK! timeDiff = ", timeDiff)
+		return
+	}
+	log.Info("timeDiff = ", timeDiff)
+
+	buttonTimes.lastMessageReceived = t
+
 	edge := "rising"
 	if evt.Type == gpiod.LineEventFallingEdge {
 		edge = "falling"
 	}
-	fmt.Printf("event:%3d %-7s %s (%s)\n",
-		evt.Offset,
-		edge,
-		t.Format(time.RFC3339Nano),
-		evt.Timestamp)
+
+	fmt.Printf("%s \n", edge)
+	// fmt.Printf("event:%3d %-7s %s (%s)\n",
+	// 	evt.Offset,
+	// 	edge,
+	// 	t.Format(time.RFC3339Nano),
+	// 	evt.Timestamp)
 }
 
 func main() {
+
+	buttonTimes.lastButtonEventType = "falling"
+	buttonTimes.lastMessageReceived = time.Now()
 
 	// hack from https://www.raspberrypi.org/forums/viewtopic.php?t=270376:
 
@@ -47,9 +77,11 @@ func main() {
 	arg3 := "in"
 
 	cmd := exec.Command(app, arg0, arg1, arg2, arg3)
-	log.Printf("Running command and waiting for it to finish...")
+	log.Printf("gpio set-up part 1")
 	err := cmd.Run()
-	log.Printf("Command finished with error: %v", err)
+	if err != nil {
+		log.Printf("Command finished with error: %v", err)
+	}
 
 	// Set up GPIO 27 with pullup resistor
 	app = "gpio"
@@ -60,12 +92,15 @@ func main() {
 	arg3 = "up"
 
 	cmd = exec.Command(app, arg0, arg1, arg2, arg3)
-	log.Printf("Running command and waiting for it to finish...")
+	log.Printf("gpio set-up part 2...")
 	err = cmd.Run()
-	log.Printf("Command finished with error: %v", err)
+	if err != nil {
+		log.Printf("Command finished with error: %v", err)
+	}
 
 	// Set up button with interrupt watch using gpiod
-	offset := rpi.J8p13
+	// offset := rpi.J8p13
+	offset := 27
 
 	c, err := gpiod.NewChip("gpiochip0")
 	if err != nil {
@@ -81,9 +116,15 @@ func main() {
 	// }
 	// defer t.Close()
 
+	// period := 10 * time.Millisecond
+	// l, _ = c.RequestLine(4, gpiod.WithDebounce(period))// during request The WithDebounce option requires Linux v5.10 or later.
+	// l.Reconfigure(gpiod.WithDebounce(period))         // once requested
+
 	l, err := c.RequestLine(offset,
 		gpiod.WithBothEdges,
-		gpiod.WithEventHandler(eventHandler))
+		gpiod.WithEventHandler(buttonEventHandler))
+
+	// NB remove pullup from the gpiod function call: requires kernel 5.5 for pullup/pulldown support.
 	if err != nil {
 		fmt.Printf("RequestLine returned error: %s\n", err)
 		if err == syscall.Errno(22) {
@@ -92,6 +133,8 @@ func main() {
 		os.Exit(1)
 	}
 	defer l.Close()
+
+	// l.Reconfigure(gpiod.WithDebounce(period)) // once requested
 
 	// In a real application the main thread would do something useful.
 	// But we'll just run for a minute then exit.
