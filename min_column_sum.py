d914284f1d17dc5b9858b340728b14d6b6e528fd
Nimrod <nimrod.gileadi@gmail.com>
2021-06-24 21:35:45 +0100

    Better docs, remove spurious prints.

--- min_column_sum.py
+++ min_column_sum.py
@@ -1,4 +1,8 @@
 #! /usr/bin/python3
+"""For each input line, decode into an array and print the smallest column sum.
+
+Lines are assumed to be 28x28 uint8 arrays, base64 encoded.
+"""
 
 import base64
 import sys
@@ -10,7 +14,5 @@ for line in sys.stdin:
         continue
     data = base64.b64decode(line.strip())
     array = np.frombuffer(data, dtype=np.uint8).reshape((28, 28))
-    print('array', array)
     sums = array.sum(axis=0, dtype=np.int64)
-    print('sums', sums)
     print(np.min(sums))
