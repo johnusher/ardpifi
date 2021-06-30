ce081712d62472974df44ce201129f0d8b1dc2aa
John Usher <yesjhon@gmail.com>
2021-06-30 20:41:21 +0200

    send all quaternion recordings to tflite model

--- test_quats_64_to_py_tf.go
+++ test_quats_64_to_py_tf.go
@@ -1,5 +1,11 @@
 package main
 
+// read in quaternion data from /letters directory, (iterate over all examples)
+// process to a 28x28 array
+// convert to base64
+// pipe to a tensorflow lite model in python
+// receive string response from tflite idicating letter match and probability
+
 import (
 	"bufio"
 	"encoding/base64"
@@ -11,15 +17,18 @@ import (
 	"math"
 	"os"
 	"os/exec"
-	"time"
+	"path/filepath"
+	"regexp"
 
 	log "github.com/sirupsen/logrus"
 	"golang.org/x/image/bmp"
 )
 
 const (
-	// quat_in     = "./letters/M/M_20-45-12/quaternion_data.txt"
-	quat_in = "./letters/O/O_20-32-50/quaternion_data.txt"
+	// quat_in = "./letters/M/M_20-45-12/quaternion_data.txt"
+	// quat_in = "./letters/O/O_20-32-50/quaternion_data.txt"
+	// quat_in = "./letters/O/O_20-44-02/quaternion_data.txt"
+	// quat_in = "./letters/O/O_20-32-50/quaternion_data.txt"
 
 	circBufferL = 600 // length of buffer where we store quat data. 600 samples @5 ms update = 3 seconds
 	lp          = 28  // pixels used to represent drawn letter, on each axis, ie lpxlp
@@ -27,7 +36,8 @@ const (
 
 func main() {
 
-	// put following on the stack
+	var quat_inFn = "./letters/M/M_20-46-09/quaternion_data.txt"
+
 	var quat_in_circ_buffer [circBufferL][5]float64   // raw quaternion inputs from file or IMU
 	var projected_circ_buffer [circBufferL][3]float64 // quat projected to xyz
 	var centre [3]float64                             // 3x1 averages of projected_circ_buffer coluns
@@ -42,292 +52,326 @@ func main() {
 	var letterImage [lp][lp]byte
 
 	var n int
-	// var numsInt byte
 
-	f, err := os.Open(quat_in)
+	searchDir := "letters"
+
+	pattern := "quaternion_data.txt"
+
+	fileList := make([]string, 0)
+	err := filepath.Walk(searchDir, func(path string, f os.FileInfo, err error) error {
+		fileList = append(fileList, path)
+		return err
+	})
+
 	if err != nil {
 		fmt.Println(err)
 	}
 
-	n = 0 // index to write into circ buffer
-	for {
+	for _, file := range fileList {
 
-		var flt1, flt2, flt3, flt4 float64
-		// var str string
+		matched, _ := regexp.MatchString(pattern, file)
+		// fmt.Println(matched, err)
 
-		fn, err := fmt.Fscan(f, &flt1, &flt2, &flt3, &flt4)
-		if fn == 0 || err != nil {
-			fmt.Printf("err %s\n", err)
-			break
+		if matched {
+			log.Printf(" file: %v", file)
+			quat_inFn = file
+
+			// log.Printf(" fileList: %v", fileList)
+
+			f, err := os.Open(quat_inFn)
+			if err != nil {
+				fmt.Println(err)
+			}
+
+			n = 0 // index to write into circ buffer
+			for {
+
+				var flt1, flt2, flt3, flt4 float64
+				// var str string
+
+				fn, err := fmt.Fscan(f, &flt1, &flt2, &flt3, &flt4)
+				if fn == 0 || err != nil {
+					// fmt.Printf("err %s\n", err)
+					break
+				}
+
+				quat_in_circ_buffer[n][0] = flt1
+				quat_in_circ_buffer[n][1] = flt2
+				quat_in_circ_buffer[n][2] = flt3
+				quat_in_circ_buffer[n][3] = flt4
+
+				// log.Printf("quat_in_circ_buffer[n][0] %v", quat_in_circ_buffer[n][0])
+				// log.Printf("quat_in_circ_buffer[n][1] %v", quat_in_circ_buffer[n][1])
+				// log.Printf("quat_in_circ_buffer[n][2] %v", quat_in_circ_buffer[n][2])
+				// log.Printf("quat_in_circ_buffer[n][3] %v", quat_in_circ_buffer[n][3])
+
+				s := quat_in_circ_buffer[n][0]
+				x := quat_in_circ_buffer[n][1]
+				y := quat_in_circ_buffer[n][2]
+				z := quat_in_circ_buffer[n][3]
+
+				projected_circ_buffer[n][0] = 1.0 - 2.0*(y*y+z*z)
+				projected_circ_buffer[n][1] = 2.0 * (x*y + s*z)
+				projected_circ_buffer[n][2] = 2.0 * (x*z - s*y)
+
+				// log.Printf("projected_circ_buffer[n][0] %v", projected_circ_buffer[n][0])
+				// log.Printf("projected_circ_buffer[n][1] %v", projected_circ_buffer[n][1])
+				// log.Printf("projected_circ_buffer[n][2] %v", projected_circ_buffer[n][2])
+
+				n = n + 1
+
+			}
+
+			// eof reached
+
+			// startTime := time.Now()
+
+			// step 3. when we have stopped recording data: average of projected
+
+			// centre := mean(projected, 1)
+
+			sumx := 0.0
+			sumz := 0.0
+			sumy := 0.0
+
+			for i := 0; i < n; i++ {
+				sumx += (projected_circ_buffer[i][0])
+				sumy += (projected_circ_buffer[i][1])
+				sumz += (projected_circ_buffer[i][2])
+			}
+
+			centre[0] = (sumx) / (float64(n))
+			centre[1] = (sumy) / (float64(n))
+			centre[2] = (sumz) / (float64(n))
+
+			// %% step 4: norm-centre
+
+			// %     centre_direction = centre' ./ norm(centre);
+
+			norm_centre := math.Sqrt(centre[0]*centre[0] + centre[1]*centre[1] + centre[2]*centre[2])
+
+			centre_direction[0] = centre[0] / norm_centre
+			centre_direction[1] = centre[1] / norm_centre
+			centre_direction[2] = centre[2] / norm_centre
+
+			// % step 5: y direction:
+			// %     y_direction = (eye[2] - centre_direction*centre_direction')* [0 1 0]';
+
+			centre_direction_sq_centre_col[0] = centre_direction[0] * centre_direction[1]
+			centre_direction_sq_centre_col[1] = centre_direction[1] * centre_direction[1]
+			centre_direction_sq_centre_col[2] = centre_direction[1] * centre_direction[2]
+
+			eye_minus_cdscc[0] = -1.0 * centre_direction_sq_centre_col[0]
+			eye_minus_cdscc[1] = 1.0 - centre_direction_sq_centre_col[1]
+			eye_minus_cdscc[2] = -1.0 * centre_direction_sq_centre_col[2]
+
+			// %     y_direction = y_direction ./ norm(y_direction);
+
+			norm_y_direction := math.Sqrt(eye_minus_cdscc[0]*eye_minus_cdscc[0] + eye_minus_cdscc[1]*eye_minus_cdscc[1] + eye_minus_cdscc[2]*eye_minus_cdscc[2])
+
+			y_direction[0] = eye_minus_cdscc[0] / norm_y_direction
+			y_direction[1] = eye_minus_cdscc[1] / norm_y_direction
+			// y_direction[1] = 1.0 // tends to unity
+			y_direction[2] = eye_minus_cdscc[2] / norm_y_direction
+
+			// %%     step 6: x_direction via cross product
+			// %     x_direction_cp = cross(centre_direction, y_direction);
+
+			x_direction[0] = centre_direction[1]*y_direction[2] - centre_direction[2]*y_direction[1]
+			// %     x_direction[1] = centre_direction[2]*y_direction[0] - centre_direction[0]*y_direction[2]; % very close to zero
+			// x_direction(2) = centre_direction(3)*y_direction(1) - centre_direction(1)*y_direction(3); % very close to zero
+			x_direction[1] = centre_direction[2]*y_direction[0] - centre_direction[0]*y_direction[2]
+			x_direction[2] = centre_direction[0]*y_direction[1] - centre_direction[1]*y_direction[0]
+
+			// %% step 7: x and y corrodinates:
+			// x(n) = x_direction(1)* projected(n, 1) + x_direction(2)* projected(n, 2)  + x_direction(3)* projected(n, 3) ;
+			// y(n) = y_direction(1)* projected(n, 1) + y_direction(2)* projected(n, 2)  + y_direction(3)* projected(n, 3) ;
+
+			minX := 1.0
+			maxX := -1.0
+			minY := minX
+			maxY := maxX
+			for i := 0; i < n; i++ {
+				// x[i] = x_direction[0]*projected_circ_buffer[i][0] + x_direction[2]*projected_circ_buffer[i][2]                               //  % x_direction(2) is so small we can ignore it
+				x[i] = x_direction[0]*projected_circ_buffer[i][0] + x_direction[1]*projected_circ_buffer[i][1] + x_direction[2]*projected_circ_buffer[i][2]
+				y[i] = y_direction[0]*projected_circ_buffer[i][0] + projected_circ_buffer[i][1] + y_direction[2]*projected_circ_buffer[i][2] // ; % y_direction(2) -> 1.0
+				minX = math.Min(minX, x[i])
+				maxX = math.Max(maxX, x[i])
+				minY = math.Min(minY, y[i])
+				maxY = math.Max(maxY, y[i])
+			}
+
+			// // % scale
+
+			absMixX := math.Abs(minX)
+			if absMixX > maxX {
+				maxX = absMixX
+			}
+
+			absMinY := math.Abs(minY)
+			if absMinY > maxY {
+				maxY = absMinY
+			}
+
+			maxdim := math.Max(maxX, maxY)
+			scaler := 0.9 / maxdim
+			// scaler := 1.0/maxdim
+			for i := 0; i < n; i++ {
+				x[i] = x[i] * scaler
+				y[i] = y[i] * scaler
+
+			}
+
+			// make black and white image:
+
+			// first blank the image:
+			for ix := 0; ix < lp; ix++ {
+				for iy := 0; iy < lp; iy++ {
+					letterImage[ix][iy] = 0
+				}
+			}
+
+			x_int := 0
+			y_int := 0
+			for i := 0; i < n; i++ {
+				x_int = int(x[i]*lp/2 + lp/2)
+				y_int = int(y[i]*lp/2 + lp/2)
+
+				// x_int = math.Max(x_int,lp/2)
+				// y_int = math.Max(y_int,lp/2)
+				letterImage[y_int][x_int] = 1
+			}
+
+			// now1 := time.Now()
+			// elapsedTime := now1.Sub(startTime)
+			// log.Printf("elapsedTimeM=%v", elapsedTime)
+
+			// log.Printf("starting python")
+			// startTime = time.Now()
+
+			// cmd := exec.Command("python", "min_column_sum.py")
+			cmd := exec.Command("python", "classifier/classify.py")
+			// cmd := exec.Command("python", "-u", "IOtest.py")
+			// cmd := exec.Command("python", "IOtest.py")
+
+			// use -u flag if we want unbuffered:
+			// https://stackoverflow.com/questions/55312593/golang-os-exec-flushing-stdin-without-closing-it
+
+			stdout, err := cmd.StdoutPipe()
+			if err != nil {
+				panic(err)
+			}
+
+			stdin, err := cmd.StdinPipe()
+			if err != nil {
+				panic(err)
+			}
+
+			err = cmd.Start()
+			if err != nil {
+				panic(err)
+			}
+
+			var joinedArray []byte
+
+			// resize matrix into long array:
+			for ix := 0; ix < lp; ix++ {
+
+				nums := letterImage[ix][:]
+				// nums := letterImage[:][ix]
+
+				joinedArray = append(joinedArray, nums...)
+				// joinedArray := bytes.Join(nums, nil)  // dunno how to do this
+			}
+
+			// log.Printf(" joinedArray: %v", joinedArray)
+
+			encoded := base64.StdEncoding.EncodeToString(joinedArray)
+
+			// encoded := string(joinedArray)
+
+			// log.Printf(" encoded: %v", encoded)
+
+			// now send to the python:
+
+			go func() {
+				defer stdin.Close()
+				io.WriteString(stdin, encoded)
+			}()
+
+			s2, err := ReadOutput(stdout)
+			if err != nil {
+				log.Printf("Process is finished ..")
+			}
+
+			// now1 = time.Now()
+			// elapsedTime = now1.Sub(startTime)
+			// log.Printf("elapsedTime TF=%v", elapsedTime)
+
+			log.Printf("raw message: %v", s2)
+
+			// f2, err := os.Create("base64Image2.txt")
+			// if err != nil {
+			// 	log.Printf("base64Image2.txt err")
+			// }
+			// defer f2.Close()
+
+			// // save encded to txt fie:
+			// n2, err := f2.WriteString(encoded)
+
+			// if err != nil {
+			// 	log.Printf("encoded writeerr")
+			// }
+			// fmt.Printf("wrote %d bytes\n", n2)
+
+			// to continuously read output from python:
+			// https://stackoverflow.com/questions/55312593/golang-os-exec-flushing-stdin-without-closing-it
+
+			// output := make(chan string)
+			// defer close(output)
+			// go ReadOutput2(output, stdout)
+
+			// log.Printf("raw message: %v", output)
+
+			// for o := range output {
+			// 	// Log(o)
+			// 	log.Printf("raw message: %v", o)
+			// }
+
+			// -------------------------------
+			// Create png image
+			img := image.NewRGBA(image.Rect(0, 0, lp, lp))
+
+			for i := 0; i < n; i++ {
+				x_int = int(x[i]*lp/2 + lp/2 + 1)
+				y_int = int(y[i]*lp/2 + lp/2 + 1)
+				img.Set(y_int, x_int, color.RGBA{255, 0, 0, 255})
+			}
+
+			// now1 = time.Now()
+			// elapsedTime = now1.Sub(startTime)
+
+			// about 200 uS
+			// log.Printf("elapsedTime1=%v", elapsedTime)
+
+			// Save to out.bmp
+			fo, err := os.OpenFile("out5.bmp", os.O_WRONLY|os.O_CREATE, 0600)
+			if err != nil {
+				log.Printf("err %s\n", err)
+			}
+
+			defer fo.Close()
+			bmp.Encode(fo, img)
+
+			// now2 := time.Now()
+			// elapsedTime = now2.Sub(now1)
+
+			// takes bout 1.5 ms to save bmp, 10 ms to save png
+			// log.Printf("elapsedTime2=%v", elapsedTime)
+
 		}
 
-		quat_in_circ_buffer[n][0] = flt1
-		quat_in_circ_buffer[n][1] = flt2
-		quat_in_circ_buffer[n][2] = flt3
-		quat_in_circ_buffer[n][3] = flt4
-
-		// log.Printf("quat_in_circ_buffer[n][0] %v", quat_in_circ_buffer[n][0])
-		// log.Printf("quat_in_circ_buffer[n][1] %v", quat_in_circ_buffer[n][1])
-		// log.Printf("quat_in_circ_buffer[n][2] %v", quat_in_circ_buffer[n][2])
-		// log.Printf("quat_in_circ_buffer[n][3] %v", quat_in_circ_buffer[n][3])
-
-		s := quat_in_circ_buffer[n][0]
-		x := quat_in_circ_buffer[n][1]
-		y := quat_in_circ_buffer[n][2]
-		z := quat_in_circ_buffer[n][3]
-
-		projected_circ_buffer[n][0] = 1.0 - 2.0*(y*y+z*z)
-		projected_circ_buffer[n][1] = 2.0 * (x*y + s*z)
-		projected_circ_buffer[n][2] = 2.0 * (x*z - s*y)
-
-		// log.Printf("projected_circ_buffer[n][0] %v", projected_circ_buffer[n][0])
-		// log.Printf("projected_circ_buffer[n][1] %v", projected_circ_buffer[n][1])
-		// log.Printf("projected_circ_buffer[n][2] %v", projected_circ_buffer[n][2])
-
-		n = n + 1
-
 	}
-
-	// eof reached
-
-	startTime := time.Now()
-
-	// step 3. when we have stopped recording data: average of projected
-
-	// centre := mean(projected, 1)
-
-	sumx := 0.0
-	sumz := 0.0
-	sumy := 0.0
-
-	for i := 0; i < n; i++ {
-		sumx += (projected_circ_buffer[i][0])
-		sumy += (projected_circ_buffer[i][1])
-		sumz += (projected_circ_buffer[i][2])
-	}
-
-	centre[0] = (sumx) / (float64(n))
-	centre[1] = (sumy) / (float64(n))
-	centre[2] = (sumz) / (float64(n))
-
-	// %% step 4: norm-centre
-
-	// %     centre_direction = centre' ./ norm(centre);
-
-	norm_centre := math.Sqrt(centre[0]*centre[0] + centre[1]*centre[1] + centre[2]*centre[2])
-
-	centre_direction[0] = centre[0] / norm_centre
-	centre_direction[1] = centre[1] / norm_centre
-	centre_direction[2] = centre[2] / norm_centre
-
-	// % step 5: y direction:
-	// %     y_direction = (eye[2] - centre_direction*centre_direction')* [0 1 0]';
-
-	centre_direction_sq_centre_col[0] = centre_direction[0] * centre_direction[1]
-	centre_direction_sq_centre_col[1] = centre_direction[1] * centre_direction[1]
-	centre_direction_sq_centre_col[2] = centre_direction[1] * centre_direction[2]
-
-	eye_minus_cdscc[0] = -1.0 * centre_direction_sq_centre_col[0]
-	eye_minus_cdscc[1] = 1.0 - centre_direction_sq_centre_col[1]
-	eye_minus_cdscc[2] = -1.0 * centre_direction_sq_centre_col[2]
-
-	// %     y_direction = y_direction ./ norm(y_direction);
-
-	norm_y_direction := math.Sqrt(eye_minus_cdscc[0]*eye_minus_cdscc[0] + eye_minus_cdscc[1]*eye_minus_cdscc[1] + eye_minus_cdscc[2]*eye_minus_cdscc[2])
-
-	y_direction[0] = eye_minus_cdscc[0] / norm_y_direction
-	y_direction[1] = eye_minus_cdscc[1] / norm_y_direction
-	// y_direction[1] = 1.0 // tends to unity
-	y_direction[2] = eye_minus_cdscc[2] / norm_y_direction
-
-	// %%     step 6: x_direction via cross product
-	// %     x_direction_cp = cross(centre_direction, y_direction);
-
-	x_direction[0] = centre_direction[1]*y_direction[2] - centre_direction[2]*y_direction[1]
-	// %     x_direction[1] = centre_direction[2]*y_direction[0] - centre_direction[0]*y_direction[2]; % very close to zero
-	// x_direction(2) = centre_direction(3)*y_direction(1) - centre_direction(1)*y_direction(3); % very close to zero
-	x_direction[1] = centre_direction[2]*y_direction[0] - centre_direction[0]*y_direction[2]
-	x_direction[2] = centre_direction[0]*y_direction[1] - centre_direction[1]*y_direction[0]
-
-	// %% step 7: x and y corrodinates:
-	// x(n) = x_direction(1)* projected(n, 1) + x_direction(2)* projected(n, 2)  + x_direction(3)* projected(n, 3) ;
-	// y(n) = y_direction(1)* projected(n, 1) + y_direction(2)* projected(n, 2)  + y_direction(3)* projected(n, 3) ;
-
-	minX := 1.0
-	maxX := -1.0
-	minY := minX
-	maxY := maxX
-	for i := 0; i < n; i++ {
-		// x[i] = x_direction[0]*projected_circ_buffer[i][0] + x_direction[2]*projected_circ_buffer[i][2]                               //  % x_direction(2) is so small we can ignore it
-		x[i] = x_direction[0]*projected_circ_buffer[i][0] + x_direction[1]*projected_circ_buffer[i][1] + x_direction[2]*projected_circ_buffer[i][2]
-		y[i] = y_direction[0]*projected_circ_buffer[i][0] + projected_circ_buffer[i][1] + y_direction[2]*projected_circ_buffer[i][2] // ; % y_direction(2) -> 1.0
-		minX = math.Min(minX, x[i])
-		maxX = math.Max(maxX, x[i])
-		minY = math.Min(minY, y[i])
-		maxY = math.Max(maxY, y[i])
-	}
-
-	// // % scale
-
-	absMixX := math.Abs(minX)
-	if absMixX > maxX {
-		maxX = absMixX
-	}
-
-	absMinY := math.Abs(minY)
-	if absMinY > maxY {
-		maxY = absMinY
-	}
-
-	maxdim := math.Max(maxX, maxY)
-	scaler := 0.9 / maxdim
-	// scaler := 1.0/maxdim
-	for i := 0; i < n; i++ {
-		x[i] = x[i] * scaler
-		y[i] = y[i] * scaler
-
-	}
-
-	// make black and white image:
-
-	// first blank the image:
-	for ix := 0; ix < lp; ix++ {
-		for iy := 0; iy < lp; iy++ {
-			letterImage[ix][iy] = 0
-		}
-	}
-
-	x_int := 0
-	y_int := 0
-	for i := 0; i < n; i++ {
-		x_int = int(x[i]*lp/2 + lp/2)
-		y_int = int(y[i]*lp/2 + lp/2)
-
-		// x_int = math.Max(x_int,lp/2)
-		// y_int = math.Max(y_int,lp/2)
-		letterImage[y_int][x_int] = 1
-	}
-
-	now1 := time.Now()
-	elapsedTime := now1.Sub(startTime)
-	// log.Printf("elapsedTimeM=%v", elapsedTime)
-
-	log.Printf("starting python")
-	startTime = time.Now()
-
-	cmd := exec.Command("python", "min_column_sum.py")
-	// cmd := exec.Command("python", "-u", "IOtest.py")
-	// cmd := exec.Command("python", "IOtest.py")
-
-	// use -u flag if we want unbuffered:
-	// https://stackoverflow.com/questions/55312593/golang-os-exec-flushing-stdin-without-closing-it
-
-	stdout, err := cmd.StdoutPipe()
-	if err != nil {
-		panic(err)
-	}
-
-	stdin, err := cmd.StdinPipe()
-	if err != nil {
-		panic(err)
-	}
-
-	err = cmd.Start()
-	if err != nil {
-		panic(err)
-	}
-
-	var joinedArray []byte
-
-	// resize matrix into long array:
-	for ix := 0; ix < lp; ix++ {
-
-		nums := letterImage[ix][:]
-		// nums := letterImage[:][ix]
-
-		joinedArray = append(joinedArray, nums...)
-		// joinedArray := bytes.Join(nums, nil)  // dunno how to do this
-	}
-
-	log.Printf(" joinedArray: %v", joinedArray)
-
-	encoded := base64.StdEncoding.EncodeToString(joinedArray)
-
-	// encoded := string(joinedArray)
-
-	log.Printf(" encoded: %v", encoded)
-
-	// now send to the python:
-
-	go func() {
-		defer stdin.Close()
-		io.WriteString(stdin, encoded)
-	}()
-
-	s2, err := ReadOutput(stdout)
-	if err != nil {
-		log.Printf("Process is finished ..")
-	}
-	log.Printf("raw message: %v", s2)
-
-	f2, err := os.Create("base64Image2.txt")
-	if err != nil {
-		log.Printf("base64Image2.txt err")
-	}
-	defer f2.Close()
-
-	n2, err := f2.WriteString(encoded)
-	// n2, err := f2.Write(joinedArray)
-
-	if err != nil {
-		log.Printf("encoded writeerr")
-	}
-	fmt.Printf("wrote %d bytes\n", n2)
-
-	// to continuously read output from python:
-	// https://stackoverflow.com/questions/55312593/golang-os-exec-flushing-stdin-without-closing-it
-
-	// output := make(chan string)
-	// defer close(output)
-	// go ReadOutput2(output, stdout)
-
-	// log.Printf("raw message: %v", output)
-
-	// for o := range output {
-	// 	// Log(o)
-	// 	log.Printf("raw message: %v", o)
-	// }
-
-	// -------------------------------
-	// Create png image
-	img := image.NewRGBA(image.Rect(0, 0, lp, lp))
-
-	for i := 0; i < n; i++ {
-		x_int = int(x[i]*lp/2 + lp/2 + 1)
-		y_int = int(y[i]*lp/2 + lp/2 + 1)
-		img.Set(y_int, x_int, color.RGBA{255, 0, 0, 255})
-	}
-
-	now1 = time.Now()
-	elapsedTime = now1.Sub(startTime)
-
-	// about 200 uS
-	log.Printf("elapsedTime1=%v", elapsedTime)
-
-	// Save to out.bmp
-	fo, err := os.OpenFile("out5.bmp", os.O_WRONLY|os.O_CREATE, 0600)
-	if err != nil {
-		log.Printf("err %s\n", err)
-	}
-
-	defer fo.Close()
-	bmp.Encode(fo, img)
-
-	now2 := time.Now()
-	elapsedTime = now2.Sub(now1)
-
-	// takes bout 1.5 ms to save bmp, 10 ms to save png
-	log.Printf("elapsedTime2=%v", elapsedTime)
 
 }
 
